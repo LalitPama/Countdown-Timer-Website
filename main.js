b4f45ffd9ae3056a3e6a6c224b2c9a57998bbceb
Lalit Kumar <110803413+LalitPama@users.noreply.github.com>
2024-07-16 23:31:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+let countdownDate = new Date("Dec 31, 2024 23:59:59").getTime();
+
+let countdownFunction = setInterval(function() {
+
+
+    let now = new Date().getTime();
+
+    
+    let distance = countdownDate - now;
+
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+  
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
